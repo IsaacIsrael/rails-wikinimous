93bed2e5782dd2f4100efeb056b6f26a4a30a8a7
isaac israel <email.iisrael@gmail.com>
2019-02-12 14:37:15 -0200

    Add dump articles to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,69 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# frozen_string_literal: true
+
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rails db:seed command (or created
+# alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#  Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Add new articles'
+Article.create(title: 'Exclusive intermediate product', content: "Aperiam harum sunt doloribus enim. Sit ut incidunt necessitatibus qui nisi ipsum in. Occaecati quia ab molestiae dolorum.
+![](http://lorempixel.com/800/300/city/)
+## Realigned multimedia website
+Error occaecati sed. Non consequuntur non. Laboriosam quaerat consequatur tempora vero excepturi.
+Voluptate reprehenderit sit. Est corporis rerum voluptates consequatur aperiam eos saepe. Unde mollitia soluta sunt laudantium eum magnam ut. Aut accusamus quo impedit aut sed incidunt commodi. Aut iste perspiciatis.
+**illum** Quisquam quia qui ea ipsa occaecati ex.Voluptas doloribus sunt nesciunt.Et sint est.
+## Upgradable web-enabled time-frame
+Consequuntur qui sint et facere praesentium dolor non.Doloribus ex debitis aut sint nihil architecto dignissimos.Alias non deleniti. [Rustic Aluminum Coat](http://darewitting.name/kyra_mclaughlin) Eum sed velit fugiat modi similique qui molestias.Ea dolor qui necessitatibus laboriosam.Excepturi sint quibusdam accusamus ipsam.
+Magnam rerum debitis. Cum esse eos sit commodi aut. Adipisci rem suscipit ut. Aliquid ut voluptatum distinctio dolore.
+Quia perspiciatis est velit doloribus impedit. Laboriosam sed eaque. Animi qui consequatur maiores impedit velit autem ab. Rerum sit pariatur beatae eligendi qui et aut.
+Voluptates porro non veniam magni similique dolorem. Eos id saepe culpa. Earum pariatur itaque consectetur hic. Voluptatum ipsa sapiente.")
+
+Article.create(title: 'Polarised encompassing structure  ', content: "Odio optio blanditiis. Quia deleniti tempora at tenetur et. Voluptate sit adipisci consequatur sit quae optio omnis. Fugit aperiam quibusdam temporibus voluptas perspiciatis molestias.
+
+![](http://lorempixel.com/800/300/city/)
+
+## Organic motivating core
+
+Dolores asperiores facilis quidem dolor in odit. Porro occaecati sed quis exercitationem consequuntur. Harum iure debitis cumque amet.
+
+Nulla dolorum nostrum tenetur. Illum vel ut enim omnis unde tenetur fugit. Adipisci repellendus voluptates unde illum doloribus. Ut ratione vel alias cumque voluptatem perferendis. Omnis nemo ut aut.
+
+**quod** Rerum perferendis voluptas ipsam inventore dolore.Nemo alias possimus et est vero quos.Ipsa vel et placeat adipisci odio.
+
+## Cross-group zero administration complexity
+
+Aut voluptate vel omnis illum.Autem repudiandae nobis qui.Facere non pariatur et qui et est consequatur. [Gorgeous Plastic Keyboard](http://paucekwehner.org/myrtle_paucek) Quia et ut nostrum perferendis consequatur in rem.Autem adipisci sed corrupti.Dolore magni autem ipsum perspiciatis modi.
+
+Minima totam dolores asperiores beatae id. Dolores enim quaerat impedit unde illum similique. Velit vero et rerum. Eveniet in cupiditate nemo qui dolores optio. Nemo temporibus eos rem ducimus rerum quis.
+
+Exercitationem nulla officia porro voluptas est id rerum. Accusamus sequi delectus. Sapiente maxime repellat fugit similique quia. Et quaerat iure doloribus saepe quo.
+
+Quo voluptatem est placeat quia voluptate id. Sed vel sint. Vitae aut ipsum quia. Sed saepe nulla laudantium molestiae voluptatem dolorem.")
+Article.create(title: 'User-friendly methodical strategy', content: "Sit eum et ipsa corrupti. Eum aut cum inventore saepe ut nulla. Dolor voluptas voluptas reprehenderit magnam non sed rerum.
+
+![](http://lorempixel.com/800/300/city/)
+
+## Exclusive national open architecture
+
+Delectus est laborum quis voluptatem aut et. Quos voluptas aut sunt necessitatibus nisi. Dolore dolores amet quia.
+
+Omnis quae et cumque. Voluptatem ipsum magnam. Et nobis occaecati dolore ipsam. Et ut quia. Ut cupiditate reiciendis nihil beatae assumenda quibusdam quis.
+
+**porro** Nobis in id architecto velit facilis dolores neque.Distinctio dolorum ut voluptatem aperiam molestiae.Eum similique voluptates omnis blanditiis doloribus recusandae.
+
+## Cloned bifurcated pricing structure
+
+Harum dicta qui modi.Nihil aut odit neque incidunt.Qui et quo veniam aperiam officiis est officia. [Mediocre Leather Keyboard](http://lockman.com/pierce_olson) Unde deserunt sint sed quis voluptas pariatur quisquam.Culpa enim impedit blanditiis eveniet dolores laboriosam sunt.Facere odit voluptatem id repellat ratione.
+
+At non hic. Vitae accusantium sequi cumque. Sit id omnis voluptas corporis iusto. Architecto iste veniam. Et dolores optio unde dolore nisi consequuntur fuga.
+
+Eaque provident qui et ut voluptatibus beatae est. Modi odio earum dolorem. Voluptates facere praesentium.
+
+Libero facere vel. Cumque velit itaque rem facere corrupti cupiditate. Aut eos enim dolores ab. Non velit quia dolorem quis fugiat.")
+
+puts 'Finish to add new articles'
